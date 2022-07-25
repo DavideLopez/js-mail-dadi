10876a2464119b3d13d8ce9011fe709608129ed9
DavideLopez <davidelopez7@gmail.com>
2022-07-25 15:38:37 +0200

    inserito html e js per il gioco dei dadi

---
+++ DADI/app.js
@@ -0,0 +1,10 @@
+console.log('dadi')
+
+//gioco dadi
+// lancia i dadi per generare un numero casuale tra 1 e 6, uno per il giocatore, uno per il computer
+// stabilire il vincitore in base al punteggio
+// possibilità di pareggio
+
+let result = document.getElementById('result')
+
+let bttn = document.getElementById('bttn')
